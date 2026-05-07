f7cf9974ae7460752a7140034229b267f4c63fd1
nathanlb14 <popgames76@gmail.com>
2026-05-07 11:32:01 +0200

    Add files via upload

--- scripts python/interface.py
+++ scripts python/interface.py
@@ -38,13 +38,13 @@ while True:
         nom = values['nom']
         prenom = values['prenom']
         liste.append(nom)
-        liste.append(prenom)
+        liste.append(prenom)  
     if event=="age eleve " :
         liste.append(2)
         window.extend_layout(window["elv"], [[sg.Input("entrer age",key='age1'),sg.Button("approuver3")]])
     if event=="approuver3":
         age1 = values['age1']
-        liste.append(age1)
+        liste.append(age1)     
     if event == "heure faite eleve" :
         liste.append(3)
         window.extend_layout(window["elv"], [[sg.Input("entrer heure faite",key='hf'),sg.Button("approuver4")]])
@@ -53,12 +53,14 @@ while True:
         liste.append(hf)      
     if event == "boite" :
         liste.append(4)
-        window.extend_layout(window["elv"], [[sg.Input("entrer boite",key='boite1'),sg.Button("approuver5")]])
-    if event=="approuver5":
-        boite1 = values['boite1']
-        liste.append(boite1)
-    
-    
+        window.extend_layout(window["elv"], [[sg.Button("manuelle1"),sg.Button("automatique1")]])
+    if event=="manuelle1":
+        liste.append("M")
+    if event=="automatique1":
+        liste.append("A")
+        
+        
+        
     if event == "modifier moniteur" :
         liste.append(2)
         window.extend_layout(window["elv"], [[sg.Input("entrer id",key='id2'),sg.Button("approuver6")]])
@@ -88,5 +90,142 @@ while True:
     if event=="approuver9":
         hf2=values['hf2']
         liste.append(hf2) 
+        
+        
+    
+    if event == "modifier voiture" :
+        liste.append(3)
+        window.extend_layout(window["elv"], [[sg.Button("nom3"),sg.Button("annee"),sg.Button("capacité"),sg.Button("kilometrage"),sg.Button("hpw"),sg.Button("lstd"),sg.Button("boite voiture")]]) 
+    if event == "nom3" :
+        liste.append(1)
+        window.extend_layout(window["elv"], [[sg.Input("entrer nom",key='nom4'),sg.Button("approuver10")]])
+    if event=="approuver10":
+        nom4 = values['nom4']
+        liste.append(nom4)
+    if event=="annee" :
+        liste.append(2)
+        window.extend_layout(window["elv"], [[sg.Input("entrer annee",key='annee1'),sg.Button("approuver11")]])
+    if event=="approuver11":
+        annee1 = values['annee1']
+        liste.append(annee1)
+    if event == "capacité" :
+         liste.append(3)
+         window.extend_layout(window["elv"], [[sg.Input("entrer la capacité",key='capacité1'),sg.Button("approuver15")]])
+    if event=="approuver15":
+         capacité1=values['capacité1']
+         liste.append(capacité1)
+    if event == "kilometrage" :
+        liste.append(4)
+        window.extend_layout(window["elv"], [[sg.Input("entrer kilometrage",key='klm'),sg.Button("approuver12")]])
+    if event=="approuver12":
+        klm1=values['klm']
+        liste.append(klm1)      
+    if event == "hpw" :
+        liste.append(5)
+        window.extend_layout(window["elv"], [[sg.Input("entrer puissance",key='hpw1'),sg.Button("approuver13")]])
+    if event=="approuver13":
+        hpw1=values['hpw1']
+        liste.append(hpw1)
+    if event == "lstd" :
+        liste.append(6)
+        window.extend_layout(window["elv"], [[sg.Input("entrer id dernier conducteur",key='lstd2'),sg.Button("approuver14")]])
+    if event=="approuver14":
+        lstd1=values['lstd2']
+        liste.append(lstd1)
+    if event == "boite voiture" :
+        liste.append(7)
+        window.extend_layout(window["elv"], [[sg.Button("manuelle2"),sg.Button("automatique2")]])
+    if event=="manuelle2":
+        liste.append("M")
+    if event=="automatique2":
+        liste.append("A")
+        
+    
+    
+    if event == "modifier heures" :
+        liste.append(4)
+        window.extend_layout(window["elv"], [[sg.Button("date"),sg.Button("duree"),sg.Button("eleve"),sg.Button("prof"),sg.Button("voiture"),sg.Button("distance"),sg.Button("depart"),sg.Button("fin"),sg.Button("boite1"),sg.Button("faite")]]) 
+    if event == "date" :
+        liste.append(1)
+        window.extend_layout(window["elv"], [[sg.Input("entrer date",key='date1'),sg.Button("approuver17")]])
+    if event=="approuver17":
+        date1 = values['date1']
+        liste.append(date1)
+        
+        
+    if event=="duree" :
+        liste.append(2)
+        window.extend_layout(window["elv"], [[sg.Input("entrer duree",key='duree1'),sg.Button("approuver18")]])
+    if event=="approuver18":
+        duree1 = values['duree1']
+        liste.append(duree1)
+        
+        
+    if event == "eleve" :
+        liste.append(3)
+        window.extend_layout(window["elv"], [[sg.Input("entrer eleve id",key='eleve1'),sg.Button("approuver19")]])
+    if event=="approuver19":
+        eleve1=values['eleve1']
+        liste.append(eleve1)   
+        
+        
+    if event == "prof" :
+        liste.append(4)
+        window.extend_layout(window["elv"], [[sg.Input("entrer prof id",key='prof1'),sg.Button("approuver20")]])
+    if event=="approuver20":
+        prof1=values['prof1']
+        liste.append(prof1)
+        
+        
+    if event == "voiture" :
+        liste.append(5)
+        window.extend_layout(window["elv"], [[sg.Input("entrer voiture id",key='voiture1'),sg.Button("approuver21")]])
+    if event=="approuver21":
+        voiture1=values['voiture1']
+        liste.append(voiture1)
+        
+        
+    if event == "distance" :
+        liste.append(6)
+        window.extend_layout(window["elv"], [[sg.Input("entrer la distance",key='distance1'),sg.Button("approuver22")]])
+    if event=="approuver22":
+        distance1=values['distance1']
+        liste.append(distance1)
+        
+        
+    if event == "depart" :
+        liste.append(7)
+        window.extend_layout(window["elv"], [[sg.Input("entrer le depart",key='depart1'),sg.Button("approuver23")]])
+    if event=="approuver23":
+        depart1=values['depart1']
+        liste.append(depart1)
+        
+        
+    if event == "fin" :
+        liste.append(8)
+        window.extend_layout(window["elv"], [[sg.Input("entrer la fin",key='fin1'),sg.Button("approuver24")]])
+    if event=="approuver24":
+        fin1=values['fin1']
+        liste.append(fin1) 
+    
+    
+    if event == "boite1" :
+        liste.append(9)
+        window.extend_layout(window["elv"], [[sg.Button("manuelle3"),sg.Button("automatique3")]])
+    if event=="manuelle3":
+        liste.append("M")
+    if event=="automatique3":
+        liste.append("A")
+        
+        
+    if event == "faite" :
+        liste.append(10)
+        window.extend_layout(window["elv"], [[sg.Button("oui"),sg.Button("non")]])
+    if event=="oui":
+        liste.append("Y")
+    if event=="non":
+        liste.append("N")
+
+   
     print(liste)
         
