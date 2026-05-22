7f38ad18f233b2cea677b8129bdb65b077dbab82
lewdalewda <101987412+lewdalewda@users.noreply.github.com>
2026-05-22 13:20:34 +0200

    Update voiture.py

--- scripts python/voiture.py
+++ scripts python/voiture.py
@@ -20,10 +20,8 @@ class voiture :
         i= int(id)
         cursor.execute("SELECT * FROM voiture WHERE elv_id = %s", (i,))
         res = cursor.fetchall()
-        
         return(res)
       
-    
     def ajouter(self): ##work
         sql = "INSERT INTO voiture(car_id, car_nom, car_annee, car_capa, car_km, car_hpw, car_lstd, car_boite) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
         cursor.execute(sql,(self.id, self.Nom, self.annee, self.capa, self.km, self.hpw, self.lstd, self.boite))
@@ -42,17 +40,12 @@ class voiture :
         cursor.execute(sql,(self.Nom, self.annee, self.capa, self.km, self.hpw, self.lstd, self.boite,self.id))
         bdd.commit()
         
-    def suprimer(id): ##work
-         
+    def suprimer(id): ##work 
         i = int(id)  
         print(i)
         sql = "DELETE FROM voiture WHERE car_id = %s"
         cursor.execute(sql,(i,))
         bdd.commit()
                 
-##p = voiture(40, "Peugeot 108", 2016, 500, 300000, 100, 0, 0)
-##p.ajouter()
-##p.lstd = 2
-##p.annee = 2012
-##p.modifier()
+
 ##bdd.close()
