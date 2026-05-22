7c3edab0011355be40b9a290b83c3f1aa5418bcf
lewdalewda <101987412+lewdalewda@users.noreply.github.com>
2026-05-22 13:20:05 +0200

    Update moniteur.py

--- scripts python/moniteur.py
+++ scripts python/moniteur.py
@@ -20,8 +20,6 @@ class moniteur :
         res = cursor.fetchall()
         return(res)
       
-    
-    
     def ajouter(self): #works
         sql = "INSERT INTO prof(pro_id, pro_nom, pro_prenom, pro_age, pro_heurescas, pro_heureslbr) VALUES(%s,%s,%s,%s,%s,%s)"
         cursor.execute(sql,(self.id, self.Nom, self.Prenom, self.age, self.hc, self.hl))
@@ -38,17 +36,11 @@ class moniteur :
         cursor.execute(sql,(self.Nom, self.Prenom, self.age, self.hc, self.hl, self.id))
         bdd.commit()
         
-    def suprimer(id): #works
-         
+    def suprimer(id): #works 
         i = int(id)  
         print(i)
         sql = "DELETE FROM prof WHERE pro_id = %s"
         cursor.execute(sql,(i,))
         bdd.commit()
-
-#p = moniteur(3,"GERAUT","Athalia",20,20,10)
-##p.ajouter()
-#p.age = 25
-#p.modifier()
-##moniteur.suprimer()
+        
 ##bdd.close()
